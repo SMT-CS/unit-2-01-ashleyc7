46dddcd2b14d98daabb9dbe039a47a605a987267
ashleyc7 <ashley.chow@stu.ocsb.ca>
2025-02-25 19:15:30 +0000

    Added circumference of a circle program

---
+++ circumferenceofcircle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Created by: Ashley Chow
+Created on: February 24 2025
+This module calculates the circumference of a circle 
+    with the diameter of 42 mm
+"""
+
+import math
+
+
+def main():
+    """
+    The main() function calculates the circumference of a circle with the diameter of 42 mm,
+    """
+
+    print("If a circle has the diameter of 42 mm,")
+    print(f"the circumference is  {(math.pi  * 42):.5f} mm.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
